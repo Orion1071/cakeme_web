8b3de02d4cc2b12012478699c6a13c5b247163f6
Sanda <sandathura2001@gmail.com>
2024-04-12 15:55:52 -0400

    add about us and menu

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
+
+// hero section
+
 $(".jumbotron").css({ height: $(window).height() + "px" });
 
 $(window).on("resize", function() {
   $(".jumbotron").css({ height: $(window).height() + "px" });
 });
+
+
+// menu section
+
+
